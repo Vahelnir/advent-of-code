fdad179ee36abb8da02205fce68e6fca854db33b
Vahelnir <Vahelnir@gmail.com>
2021-02-08 17:37:58 +0100

    Second half of day 2

--- aoc-2/index.ts
+++ aoc-2/index.ts
@@ -1,14 +1,14 @@
-type ParsedEntry {
+type ParsedEntry = {
   letter: string;
   range: [number, number];
   password: string;
-}
+};
 
-function parseLine(textLine: string) {
+function parseLine(textLine: string): ParsedEntry {
   const [validationHeader, password] = textLine.split(":");
   const [rangeString, letter] = validationHeader.split(" ");
-  const range = rangeString.split("-").map((value) => parseInt(value, 10));
-  return { letter, range, password };
+  const [min, max] = rangeString.split("-").map((value) => parseInt(value, 10));
+  return { letter, range: [min, max], password };
 }
 
 function oldJobValidation({ letter, password, range }: ParsedEntry): boolean {
@@ -20,11 +20,28 @@ function oldJobValidation({ letter, password, range }: ParsedEntry): boolean {
   return letterMatches <= max && letterMatches >= min;
 }
 
+function officialTobogganValidation({
+  letter,
+  password,
+  range,
+}: ParsedEntry): boolean {
+  const [firstPosition, secondPosition] = range;
+  return (
+    (password[firstPosition] === letter ||
+      password[secondPosition] === letter) &&
+    password[firstPosition] !== password[secondPosition]
+  );
+}
+
 async function start() {
-  const fileContent = await Deno.readTextFile("input.txt");
+  const filePath = new URL("input.txt", import.meta.url);
+  const fileContent = await Deno.readTextFile(filePath);
   const entries = fileContent.split("\n").map(parseLine);
-  const validEntries = entries.filter(oldJobValidation);
-  console.log(validEntries.length);
+  console.log("Old job validation:", entries.filter(oldJobValidation).length);
+  console.log(
+    "Official Toboggan Corporate Policy validation:",
+    entries.filter(officialTobogganValidation).length
+  );
 }
 
 await start();
