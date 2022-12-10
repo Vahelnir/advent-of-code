217f48a329b684d871d4bb190f305a8ce4ed1681
Vahelnir <vahelnir@gmail.com>
2022-12-11 00:54:42 +0100

    try solving day 9 part 2, doesn't work tho

--- src/days/9/index.ts
+++ src/days/9/index.ts
@@ -9,10 +9,11 @@ type Position = {
 };
 
 type State = {
-  head_position: Position;
-  tail_position: Position;
+  positions: Position[];
   tail_visited_position: Position[];
 };
+
+const ROPE_SIZE = 10;
 
 function is_direction(str: string): str is Direction {
   return directions.includes(str);
@@ -48,33 +49,44 @@ function move({ x, y }: Position, direction: Direction): Position {
 }
 
 function tick(state: State, direction: Direction): State {
-  const {
-    head_position,
-    tail_position,
-    tail_visited_position: tail_different_position,
-  } = state;
+  const { positions, tail_visited_position: tail_different_position } = state;
 
+  const [head_position, ...rest_positions] = positions;
   const new_head_position = move(head_position, direction);
 
-  let new_tail_position = tail_position;
-  if (
-    are_position_equal(head_position, tail_position) ||
-    are_position_equal(new_head_position, tail_position)
-  ) {
-    new_tail_position = tail_position;
-  } else if (
-    !are_position_adjacent(new_head_position, tail_position) &&
-    !are_position_diagonal(new_head_position, tail_position)
-  ) {
-    new_tail_position = head_position;
+  let previous_position = head_position;
+  let previous_new_position = new_head_position;
+  let new_rest_positions = [];
+  for (let i = 0; i < rest_positions.length; i++) {
+    const current_position = rest_positions[i];
+    let new_current_position = current_position;
+    if (
+      are_position_equal(previous_position, current_position) ||
+      are_position_equal(previous_new_position, current_position)
+    ) {
+      new_current_position = current_position;
+    } else if (
+      !are_position_adjacent(previous_new_position, current_position) &&
+      !are_position_diagonal(previous_new_position, current_position)
+    ) {
+      new_current_position = previous_position;
+    }
+
+    previous_position = current_position;
+    previous_new_position = new_current_position;
+    new_rest_positions.push(new_current_position);
   }
 
+  const new_positions = [new_head_position, ...new_rest_positions];
+
   return {
-    tail_visited_position: !are_position_equal(tail_position, new_tail_position)
-      ? [...tail_different_position, new_tail_position]
+    tail_visited_position: !are_position_equal(
+      positions[ROPE_SIZE - 1],
+      new_positions[ROPE_SIZE - 1]
+    )
+      ? [...tail_different_position, new_positions[ROPE_SIZE - 1]]
       : tail_different_position,
-    head_position: new_head_position,
-    tail_position: new_tail_position,
+    positions: new_positions,
   };
 }
 
@@ -105,32 +117,36 @@ export const run: DayEntryPoint = (input) => {
 
   const default_position = { x: 0, y: 0 };
   let state: State = {
-    head_position: default_position,
-    tail_position: default_position,
+    positions: Array(ROPE_SIZE).fill(default_position),
     tail_visited_position: [default_position],
   };
 
   let current_tick = 0;
   let max_x = 0;
   let max_y = 0;
+  let min_x = 0;
+  let min_y = 0;
   for (const line of lines) {
     const [direction, amount] = get_movement_instruction(line);
+    console.log(direction, amount);
     for (let i = 0; i < amount; i++) {
       state = tick(state, direction);
+      draw_grid_with_rope(state.positions, 5, 4, 0, 0);
+      console.log("");
 
-      // console.log(
-      //   direction,
-      //   current_tick,
-      //   state.tail_position,
-      //   state.head_position
-      // );
-
-      // if (max_x < state.head_position.x) {
-      //   max_x = state.head_position.x;
-      // }
-      // if (max_y < state.head_position.y) {
-      //   max_y = state.head_position.y;
-      // }
+      const head_position = state.positions[0];
+      if (max_x < head_position.x) {
+        max_x = head_position.x;
+      }
+      if (max_y < head_position.y) {
+        max_y = head_position.y;
+      }
+      if (min_x > head_position.x) {
+        min_x = head_position.x;
+      }
+      if (min_y > head_position.y) {
+        min_y = head_position.y;
+      }
       current_tick++;
     }
   }
@@ -144,18 +160,60 @@ export const run: DayEntryPoint = (input) => {
     ).length
   );
 
-  // console.log(max_x, max_y);
-  // const grid = Array(max_y + 1)
-  //   .fill(".")
-  //   .map(() => Array(max_x + 1).fill("."));
-  // for (const { x, y } of state.tail_visited_position) {
-  //   console.log(x, y);
-  //   grid[y][x] = "#";
-  // }
-  // console.log(
-  //   grid
-  //     .map((row) => row.join(""))
-  //     .reverse()
-  //     .join("\n")
-  // );
+  const offset_x = Math.abs(min_x);
+  const offset_y = Math.abs(min_y);
+  console.log(max_x, max_y, offset_x, offset_y);
+  draw_visited_grid(
+    state.tail_visited_position,
+    max_x,
+    max_y,
+    offset_x,
+    offset_y
+  );
 };
+
+function draw_visited_grid(
+  tail_visited_position: Position[],
+  max_x: number,
+  max_y: number,
+  offset_x: number,
+  offset_y: number
+) {
+  const grid = Array(max_y + 1 + offset_y)
+    .fill(".")
+    .map(() => Array(max_x + 1 + offset_x).fill("."));
+  for (const { x, y } of tail_visited_position) {
+    grid[y + offset_y][x + offset_x] = "#";
+  }
+  console.log(
+    grid
+      .map((row) => row.join(""))
+      .reverse()
+      .join("\n")
+  );
+}
+
+function draw_grid_with_rope(
+  rope_positions: Position[],
+  max_x: number,
+  max_y: number,
+  offset_x: number,
+  offset_y: number
+) {
+  const grid = Array(max_y + 1 + offset_y)
+    .fill(".")
+    .map(() => Array(max_x + 1 + offset_x).fill("."));
+  for (let i = 0; i < rope_positions.length; i++) {
+    const { x, y } = rope_positions[i];
+    if (grid[y + offset_y][x + offset_x] !== ".") {
+      continue;
+    }
+    grid[y + offset_y][x + offset_x] = i === 0 ? "H" : i;
+  }
+  console.log(
+    grid
+      .map((row) => row.join(""))
+      .reverse()
+      .join("\n")
+  );
+}
