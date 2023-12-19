9c5cec9974c719582be4c639a3b00ebd8192e334
Vahelnir <vahelnir@gmail.com>
2023-12-20 00:48:42 +0100

    wip day 19 part 2, only missing simplifying intervals

--- src/days/19/index.ts
+++ src/days/19/index.ts
@@ -13,9 +13,12 @@ type Workflow = {
   name: string;
   conditions: ConditionStatement[];
 };
-
-const isStatementKey = (str: string): str is PartKey =>
-  ALLOWED_STATEMENT_KEYS.includes(str as PartKey);
+type Boundary = { min: number; max: number };
+type StackElement = {
+  boundaries: Record<PartKey, Boundary>;
+  workflow: Workflow;
+  visitedWorkflows: Set<string>;
+};
 
 // a<2006:qkq
 const parseCondition = (rawStatement: string): ConditionStatement => {
@@ -57,14 +60,14 @@ const parsePart = (rawPart: string) => {
     .map((entry) => entry.split("="))
     .reduce(
       (part, [key, value]) =>
-        ({ ...part, [key]: Number(value) } satisfies Part),
-      {} as Part
+        ({ ...part, [key]: Number(value) }) satisfies Part,
+      {} as Part,
     );
 };
 
 const evaluateCondition = (
   condition: NonNullable<ConditionStatement["condition"]>,
-  part: Part
+  part: Part,
 ) => {
   if (condition.operator === ">") {
     return part[condition.key] > condition.value;
@@ -72,29 +75,207 @@ const evaluateCondition = (
   return part[condition.key] < condition.value;
 };
 
+function validatePart(
+  workflowMap: Map<string, Workflow>,
+  part: {
+    [p: string]: number;
+    a: number;
+    s: number;
+    x: number;
+    m: number;
+  },
+) {
+  let currentWorkflowName: string | undefined = "in";
+  while (currentWorkflowName) {
+    if ("AR".includes(currentWorkflowName)) {
+      return currentWorkflowName;
+    }
+
+    const currentWorkflow = workflowMap.get(currentWorkflowName);
+    if (!currentWorkflow) {
+      throw new Error("no workflow with that name");
+    }
+
+    for (const condition of currentWorkflow.conditions) {
+      if (
+        condition.condition &&
+        !evaluateCondition(condition.condition, part)
+      ) {
+        continue;
+      }
+
+      currentWorkflowName = condition.next;
+      break;
+    }
+  }
+}
+
+const isOverlapping = (a: Boundary, b: Boundary) => {
+  return (
+    (a.min >= b.min && a.min <= b.max) || (a.max <= b.max && a.max >= b.min)
+  );
+};
+const isSubset = (a: Boundary, b: Boundary) => {
+  return a.min >= b.min && a.min <= b.max && a.max <= b.max && a.max >= b.min;
+};
+
+const tryUnion = (a: Boundary, b: Boundary) => {
+  if (!isOverlapping(a, b) && !isOverlapping(b, a)) {
+    return undefined;
+  }
+
+  // console.log("a", a.min, a.max, b.min, b.max);
+  // console.log("b", Math.max(a.max, b.max), Math.min(a.min, b.min));
+  return { max: Math.max(a.max, b.max), min: Math.min(a.min, b.min) };
+};
+
 export const run: DayEntryPoint = (input) => {
   const [rawWorkflows, rawParts] = input
     .split("\n\n")
     .map((fragment) => fragment.split("\n"));
-  const workflows = rawWorkflows.map(parseWorkflow);
+  const workflowMap = rawWorkflows
+    .map(parseWorkflow)
+    .reduce((map, workflow) => {
+      map.set(workflow.name, workflow);
+      return map;
+    }, new Map<string, Workflow>());
   const parts = rawParts.map(parsePart);
-  const ﬁrstWorkflow = workflows[0];
-  const workflowMap = workflows.reduce((map, workflow) => {
-    map.set(workflow.name, workflow);
-    return map;
-  }, new Map<string, Workflow>());
 
-  for (const part of parts) {
-    let currentWorkflow: Workflow | undefined = ﬁrstWorkflow;
-    while (currentWorkflow) {
-      for (const condition of currentWorkflow.conditions) {
-        if (
-          !condition.condition ||
-          (condition.condition && evaluateCondition(condition.condition, part))
-        ) {
-          currentWorkflow = workflowMap.get(condition.next);
-        }
-      }
-    }
+  const res = parts
+    .filter((part) => validatePart(workflowMap, part) === "A")
+    .reduce((acc, { a, m, s, x }) => acc + a + m + s + x, 0);
+  console.log("first", res);
+
+  const initialWorkflow = workflowMap.get("in");
+  if (!initialWorkflow) {
+    throw new Error("no initial workflow");
   }
+
+  const boundariesList: StackElement["boundaries"][] = [];
+
+  const defaultBoundary = { min: 1, max: 4000 };
+  const stack: StackElement[] = [
+    {
+      boundaries: {
+        x: { ...defaultBoundary },
+        m: { ...defaultBoundary },
+        a: { ...defaultBoundary },
+        s: { ...defaultBoundary },
+      },
+      workflow: initialWorkflow,
+      visitedWorkflows: new Set<string>(),
+    },
+  ];
+  while (stack.length > 0) {
+    const element = stack.pop();
+    if (!element || element.visitedWorkflows.has(element.workflow.name)) {
+      continue;
+    }
+
+    const { visitedWorkflows, boundaries, workflow } = element;
+
+    const newVisitedWorkflow = new Set([...visitedWorkflows, workflow.name]);
+    const nextWorkflows = workflow.conditions.map((condition) => {
+      if (condition.next === "A") {
+        boundariesList.push(boundaries);
+        return undefined;
+      }
+      if (condition.next === "R") {
+        return undefined;
+      }
+      const nextWorkflow = workflowMap.get(condition.next);
+      if (!nextWorkflow) {
+        throw new Error(`no workflow with this name "${condition.next}"`);
+      }
+
+      if (condition.condition) {
+        const cond = condition.condition;
+        const operator = cond.operator;
+        if (operator === ">") {
+          return {
+            boundaries: {
+              ...boundaries,
+              [cond.key]: {
+                ...boundaries[cond.key],
+                min: Math.max(cond.value, boundaries[cond.key].min),
+              },
+            },
+            workflow: nextWorkflow,
+            visitedWorkflows: newVisitedWorkflow,
+          };
+        }
+
+        return {
+          boundaries: {
+            ...boundaries,
+            [cond.key]: {
+              ...boundaries[cond.key],
+              max: Math.min(cond.value, boundaries[cond.key].max),
+            },
+          },
+          workflow: nextWorkflow,
+          visitedWorkflows: newVisitedWorkflow,
+        };
+      }
+
+      return {
+        boundaries,
+        workflow: nextWorkflow, // because yolo
+        visitedWorkflows: new Set([...visitedWorkflows, workflow.name]),
+      } satisfies StackElement;
+    });
+
+    stack.push(
+      ...nextWorkflows.filter((el): el is StackElement => el !== null),
+    );
+  }
+
+  const testBoundaries: StackElement["boundaries"][] = [];
+  for (const boundaries of boundariesList.filter(
+    // remove duplicates
+    (boundaries1, index) =>
+      index ===
+      boundariesList.findIndex((boundaries2) =>
+        Bun.deepEquals(boundaries1, boundaries2, true),
+      ),
+  )) {
+    const unions = boundariesList.map((boundaries2) => ({
+      x: tryUnion(boundaries.x, boundaries2.x) ?? boundaries.x,
+      m: tryUnion(boundaries.m, boundaries2.m) ?? boundaries.m,
+      a: tryUnion(boundaries.a, boundaries2.a) ?? boundaries.a,
+      s: tryUnion(boundaries.s, boundaries2.s) ?? boundaries.s,
+    }));
+    testBoundaries.push(...unions);
+  }
+  console.log(
+    boundariesList.filter(
+      // remove duplicates
+      (boundaries1, index) =>
+        index ===
+        boundariesList.findIndex((boundaries2) =>
+          Bun.deepEquals(boundaries1, boundaries2, true),
+        ),
+    ),
+  );
+  console.log(
+    "second",
+    testBoundaries
+      .filter(
+        // remove duplicates
+        (boundaries1, index) =>
+          index ===
+          boundariesList.findIndex((boundaries2) =>
+            Bun.deepEquals(boundaries1, boundaries2, true),
+          ),
+      )
+      .reduce((acc, { x, m, a, s }) => {
+        return (
+          acc +
+          (x.max - x.min - 1) *
+            (m.max - m.min - 1) *
+            (a.max - a.min - 1) *
+            (s.max - s.min - 1)
+        );
+      }, 0),
+  );
 };
