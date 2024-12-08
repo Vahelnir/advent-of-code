bb8c9a5adcfffabe2573cbb6311bf6ae4ca2904a
Vahelnir <vahelnir@gmail.com>
2024-12-08 17:10:32 +0100

    solve day 8 part two

--- src/days/8/index.ts
+++ src/days/8/index.ts
@@ -39,7 +39,49 @@ function solvePartOne(grid: string[][], antennas: Antenna[]) {
   }
 
   console.log(finalGrid.map((row) => row.join("")).join("\n"));
-  console.log(antinodes.size);
+  console.log("part one:", antinodes.size);
+}
+
+function solvePartTwo(grid: string[][], antennas: Antenna[]) {
+  const antinodes = new Set<string>();
+  for (const antenna of antennas) {
+    antinodes.add(`${antenna.position.y},${antenna.position.x}`);
+    const similarAntennas = antennas.filter(
+      (similarAntenna) =>
+        similarAntenna.id === antenna.id &&
+        similarAntenna.position.x !== antenna.position.x &&
+        similarAntenna.position.y !== antenna.position.y
+    );
+    for (const similarAntenna of similarAntennas) {
+      const dx = similarAntenna.position.x - antenna.position.x;
+      const dy = similarAntenna.position.y - antenna.position.y;
+      let elongatedPosition = {
+        x: similarAntenna.position.x + dx,
+        y: similarAntenna.position.y + dy,
+      };
+      while (
+        elongatedPosition.x >= 0 &&
+        elongatedPosition.y >= 0 &&
+        elongatedPosition.x < grid[0].length &&
+        elongatedPosition.y < grid.length
+      ) {
+        antinodes.add(`${elongatedPosition.y},${elongatedPosition.x}`);
+        elongatedPosition = {
+          x: elongatedPosition.x + dx,
+          y: elongatedPosition.y + dy,
+        };
+      }
+    }
+  }
+
+  const finalGrid = grid.map((row) => row.map((cell) => cell));
+  for (const antinode of antinodes) {
+    const [y, x] = antinode.split(",").map(Number);
+    finalGrid[y][x] = "#";
+  }
+
+  console.log(finalGrid.map((row) => row.join("")).join("\n"));
+  console.log("part two:", antinodes.size);
 }
 
 export const run: DayEntryPoint = (input) => {
@@ -51,4 +93,5 @@ export const run: DayEntryPoint = (input) => {
     )
     .filter(({ id }) => id !== ".");
   solvePartOne(grid, antennas);
+  solvePartTwo(grid, antennas);
 };
