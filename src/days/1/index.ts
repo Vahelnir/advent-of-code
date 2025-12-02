c91f30002864971155a48947a23bbff39996fde6
vhamayon <vhamayon@knowmore.fr>
2025-12-02 08:46:12 +0100

    unfinished day 1 part 2 improvement

--- src/days/1/index.ts
+++ src/days/1/index.ts
@@ -1,4 +1,5 @@
 import type { DayEntryPoint } from "../../types/DayEntryPoint";
+import expectedValues from "./compare_expected_values.json" assert { type: "json" };
 
 const MAX_DIAL_VALUE = 100;
 const START_DIAL_VALUE = 50;
@@ -11,30 +12,42 @@ function circularMod(value: number, add: number, max: number) {
   return result;
 }
 
-export const run: DayEntryPoint = (input) => {
+export const run: DayEntryPoint = async (input) => {
   const values = input.split("\n").map((line) => {
     const result = /(L|R)(\d+)/.exec(line);
     if (!result) {
       throw new Error("Invalid instruction: " + line);
     }
 
-    const [, direction, value] = result;
-    const isNegative = direction === "L";
-    return Number(value) * (isNegative ? -1 : 1);
+    const isNegative = result[1] === "L";
+    return Number(result[2]) * (isNegative ? -1 : 1);
   });
 
-  let foundZeros = 0;
+  let i = 0;
+  let totalFoundZeros = 0;
   let position = START_DIAL_VALUE;
   for (const value of values) {
-    for (let step = 0; step < Math.abs(value); step++) {
-      const stepValue = value > 0 ? 1 : -1;
-      position = circularMod(position, stepValue, MAX_DIAL_VALUE);
-      if (position === 0) {
-        foundZeros++;
-      }
+    const expectedValue = expectedValues[i++];
+    const previousPosition = position;
+    position = circularMod(position, value, MAX_DIAL_VALUE);
+
+    let foundZeros = 0;
+    const sum = value + previousPosition;
+    if (value < 0 && sum < 0) {
+      foundZeros += 1 + Math.floor(Math.abs(sum) / MAX_DIAL_VALUE);
     }
-    console.log(position);
+    if (value > 0 && sum - MAX_DIAL_VALUE >= 0) {
+      const rest = sum - MAX_DIAL_VALUE;
+      foundZeros += 1 + Math.floor(rest / MAX_DIAL_VALUE);
+    }
+    if (expectedValue !== foundZeros) {
+      throw new Error(
+        `Unexpected number of zeros found for move ${value} from position ${previousPosition}. Expected ${expectedValue}, got ${foundZeros}`,
+      );
+    }
+    console.log(previousPosition, value, foundZeros, "-->", position, sum);
+    totalFoundZeros += foundZeros;
   }
 
-  console.log("Day 1 Part 1:", foundZeros);
+  console.log("Day 1 Part 1:", totalFoundZeros);
 };
