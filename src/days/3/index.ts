2e0cd00f281c2e878ce9e6447afdb678233a3e54
vhamayon <vhamayon@knowmore.fr>
2025-12-03 10:26:28 +0100

    solve day 3 part 2

--- src/days/3/index.ts
+++ src/days/3/index.ts
@@ -1,40 +1,53 @@
 import type { DayEntryPoint } from "../../types/DayEntryPoint";
 
-export const run: DayEntryPoint = async (input) => {
-  const banks = input.split("\n").map((line) => line.split("").map(Number));
-
-  let outputs: number[] = [];
-  for (const bank of banks) {
-    const sortedBank = bank.toSorted((a, b) => b - a);
-    let highest = sortedBank[0];
+export function findHighestDigitInBank(
+  bank: number[],
+  remainingDigits: number,
+) {
+  const sortedBank = bank.toSorted((a, b) => b - a);
+  for (let i = 0; i < sortedBank.length; i++) {
+    let highest = sortedBank[i];
     if (!highest) {
       throw new Error(`no highest found in bank: ${bank}`);
     }
 
-    let highestIndex = bank.indexOf(highest);
-    if (highestIndex + 1 === bank.length) {
-      highest = sortedBank[1];
-      if (!highest) {
-        throw new Error(`no highest found in bank: ${bank}`);
-      }
-
-      highestIndex = bank.indexOf(highest);
+    const highestIndex = bank.indexOf(highest);
+    const remainingLength = bank.length - (highestIndex + 1);
+    if (remainingLength >= remainingDigits) {
+      return { highest, highestIndex };
     }
-
-    console.log(highestIndex, bank.length);
-    const [secondHighest] = bank
-      .slice(highestIndex + 1)
-      .toSorted((a, b) => b - a);
-    if (!secondHighest) {
-      throw new Error(`no second highest found in bank: ${bank}`);
-    }
-
-    outputs.push(highest * 10 + secondHighest);
   }
 
-  console.log(
-    "part 1:",
-    outputs,
-    outputs.reduce((a, b) => a + b, 0),
-  );
+  throw new Error(`no highest found in bank: ${bank}`);
+}
+
+function findBankJoltage(banks: number[][], digitCount: number) {
+  let outputs: number[] = [];
+  for (const bank of banks) {
+    const digits: number[] = [];
+    let bankQueue = [...bank];
+    for (let i = 0; i < digitCount; i++) {
+      const { highest, highestIndex } = findHighestDigitInBank(
+        bankQueue,
+        digitCount - i - 1,
+      );
+      bankQueue = bankQueue.slice(highestIndex + 1);
+      digits.push(highest);
+    }
+
+    const output = digits.reduce(
+      (acc, digit, index) => acc + digit * 10 ** (digitCount - index - 1),
+      0,
+    );
+    outputs.push(output);
+  }
+
+  return outputs.reduce((a, b) => a + b, 0);
+}
+
+export const run: DayEntryPoint = async (input) => {
+  const banks = input.split("\n").map((line) => line.split("").map(Number));
+
+  console.log("part 1:", findBankJoltage(banks, 2));
+  console.log("part 2:", findBankJoltage(banks, 12));
 };
