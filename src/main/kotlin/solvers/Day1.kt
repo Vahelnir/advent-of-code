d677547c2a6ad625977df902518542467d613a98
Vahelnir <vahelnir@gmail.com>
2025-12-18 00:15:06 +0100

    solve day 1 part 1

--- src/main/kotlin/solvers/Day1.kt
+++ src/main/kotlin/solvers/Day1.kt
@@ -1,8 +1,16 @@
 package fr.vahelnir.solvers
 
 import fr.vahelnir.DaySolver
+import kotlin.math.floor
 
 class Day1 : DaySolver {
     override fun solve(input: List<String>) {
+        val sum = input
+            .filter { it.isNotEmpty() }
+            .map { it.toInt() }
+            .map { floor((it / 3).toDouble()) - 2 }
+            .reduce { acc, d -> acc + d }
+
+        println("Day 1 - Part 1 : $sum")
     }
 }
