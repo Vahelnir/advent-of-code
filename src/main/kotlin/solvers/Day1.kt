bdf3d14e359e72b7c58bdeb5268184acb58905e8
Vahelnir <vahelnir@gmail.com>
2025-12-18 00:22:35 +0100

    solve day 1 part 2

--- src/main/kotlin/solvers/Day1.kt
+++ src/main/kotlin/solvers/Day1.kt
@@ -8,9 +8,28 @@ class Day1 : DaySolver {
         val sum = input
             .filter { it.isNotEmpty() }
             .map { it.toInt() }
-            .map { floor((it / 3).toDouble()) - 2 }
+            .map { calculateFuel(it) }
             .reduce { acc, d -> acc + d }
 
         println("Day 1 - Part 1 : $sum")
+
+        val totalSum = input
+            .filter { it.isNotEmpty() }
+            .map { it.toInt() }
+            .map { calculateTotalFuel(it) }
+            .reduce { acc, d -> acc + d }
+        println("Day 1 - Part 2 : $totalSum")
+    }
+
+    fun calculateFuel(mass: Int): Int = floor((mass / 3).toDouble()).toInt() - 2
+    fun calculateTotalFuel(mass: Int): Int {
+        var totalFuel = 0
+        var fuel = calculateFuel(mass)
+        while (fuel > 0) {
+            totalFuel += fuel
+            fuel = calculateFuel(fuel)
+        }
+
+        return totalFuel
     }
 }
